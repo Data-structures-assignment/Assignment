df7e91224f8d027517230bb048bb9e89f01efc4b
Joy Shaney <opiyojoyshaney@gmail.com>
2024-09-29 23:25:17 +0300

    Update array-with_stack.cpp

--- array-with_stack.cpp
+++ array-with_stack.cpp
@@ -1,1 +1,22 @@
-#include
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> operations;
+        int current = 1;
+        
+        for (int num : target) {
+            // Push all numbers between current and num
+            while (current < num) {
+                operations.push_back("Push");
+                operations.push_back("Pop");
+                current++;
+            }
+            
+            // Push the target number
+            operations.push_back("Push");
+            current++;
+        }
+        
+        return operations;
+    }
+};
