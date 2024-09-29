f563c6ffecb00f70770763a94a29684e67402577
Joy Shaney <opiyojoyshaney@gmail.com>
2024-09-29 23:10:11 +0300

    Update reverse_integer.cpp

--- reverse_integer.cpp
+++ reverse_integer.cpp
@@ -1,1 +1,19 @@
-#include
+class Solution {
+    public int reverse(int x) {
+         int reversed = 0;
+        
+        while (x != 0) {
+            int digit = x % 10;
+            
+            // Check for overflow before adding new digit
+            if (reversed > 214748364 || (reversed == 214748364 && digit > 7)) return 0;
+            if (reversed < -214748364 || (reversed == -214748364 && digit < -8)) return 0;
+            
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        
+        return reversed;
+        
+    }
+}
